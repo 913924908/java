a427c8fcd2128dd5969cf92a0b709685940bcd98
913924908 <33998339+913924908@users.noreply.github.com>
2020-02-27 22:00:43 +0800

    Update User.java

--- login/User.java
+++ login/User.java
@@ -1,1 +1,58 @@
 
+public class User {
+    private String username;
+    private String pwd;
+    private String sex;
+    private int age;
+    public User(){
+
+    }
+    public User(String username, String pwd, String sex, int age) {
+        this.username = username;
+        this.pwd = pwd;
+        this.sex = sex;
+        this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", pwd='" + pwd + '\'' +
+                ", sex='" + sex + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
